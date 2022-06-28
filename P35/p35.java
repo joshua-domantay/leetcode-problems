efa248512fd2d8dc9f8a38a9c4edb36b6dd6f5e0
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2022-06-27 21:50:32 -0700

    Finish Problem 35 (Search Insert Position) in Java

---
+++ P35/p35.java
@@ -0,0 +1,19 @@
+public class p35 {
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 2));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 7));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        for(int i = 0; i < 4; i++) {
+            int mid = (low + high) / 2;
+            if(nums[mid] < target) { low = mid; }
+            else { high = mid; }
+        }
+        for(int i = low; i <= high; i++) {
+            if(nums[i] >= target) { return i; }
+        }
+        return high + 1;
+    }
+}
