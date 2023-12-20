cd50f65ebaed1c63f8bdcacc4091035c53b3a30e
Josh <dom.josh2701@gmail.com>
2023-12-19 18:25:16 -0800

    Solve Problem 961 - N-Repeated Element in Size 2N Array

--- P_0961/p_0961.java
+++ P_0961/p_0961.java
@@ -1,5 +1,20 @@
+import java.util.HashMap;
+
 public class p_0961 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(repeatedNTimes(new int[]{5,1,5,2,5,3,5,4}));
+    }
+
+    // Using HashSet may improve memory usage according to better user submissions
+    public static int repeatedNTimes(int[] nums) {
+        HashMap m = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            if(m.containsKey(nums[i])) {
+                return nums[i];
+            } else {
+                m.put(nums[i], 1);
+            }
+        }
+        return -1;
     }
 }
