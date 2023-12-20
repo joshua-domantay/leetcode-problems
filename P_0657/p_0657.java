3c11c2ff8c4451b5e81a9e3b0c2cbe65bb653133
Josh <dom.josh2701@gmail.com>
2023-12-19 18:43:58 -0800

    Solve Problem 657 - Robot Return to Origin

--- P_0657/p_0657.java
+++ P_0657/p_0657.java
@@ -1,5 +1,29 @@
 public class p_0657 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(judgeCircle("UD"));
+    }
+
+    // User submissions -> Maps for better runtime, moves.toCharArray for memory
+    public static boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+
+        for(int i = 0; i < moves.length(); i++) {
+            switch(moves.charAt(i)) {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+                default:    // L
+                    x--;
+            }
+        }
+
+        return (x == 0) && (y == 0);
     }
 }
