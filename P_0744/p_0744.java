679d68881f7c7a7fbb5b1a9472ad5fad2cc27274
Josh <dom.josh2701@gmail.com>
2023-12-20 22:57:55 -0800

    Solve Problem 744 - Find Smallest Letter Greater Than Target

--- P_0744/p_0744.java
+++ P_0744/p_0744.java
@@ -1,5 +1,20 @@
 public class p_0744 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(nextGreatestLetter(new char[]{'x','x','f','c','y','y'}, 'c'));
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        char result = '~';
+        for(char x : letters) {
+            if((x != target) && ((x - target) > 0)) {
+                if(x < result) {
+                    result = x;
+                }
+            }
+        }
+        if(result == '~') {
+            return letters[0];
+        }
+        return result;
     }
 }
