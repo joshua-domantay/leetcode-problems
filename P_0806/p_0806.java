b8bb9bb2f320020597d0e8a1f236fd3a874fa843
Josh <dom.josh2701@gmail.com>
2023-12-20 19:34:37 -0800

    Solve Problem 806 - Number of Lines To Write String

--- P_0806/p_0806.java
+++ P_0806/p_0806.java
@@ -1,5 +1,18 @@
 public class p_0806 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] result = numberOfLines(new int[]{4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}, "bbbcccdddaaa");
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+
+    public static int[] numberOfLines(int[] widths, String s) {
+        int[] result = new int[]{1, 0};
+        for(char c : s.toCharArray()) {
+            if((result[1] + widths[c - 'a']) > 100) {
+                result[0]++;
+                result[1] = 0;
+            }
+            result[1] += widths[c - 'a'];
+        }
+        return result;
     }
 }
