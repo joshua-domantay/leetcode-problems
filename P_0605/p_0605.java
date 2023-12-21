b5d298a2dd72a1d48c012638b190b17c84fab9f9
Josh <dom.josh2701@gmail.com>
2023-12-20 19:58:11 -0800

    Solve Problem 605 - Can Place Flowers

--- P_0605/p_0605.java
+++ P_0605/p_0605.java
@@ -1,5 +1,26 @@
 public class p_0605 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1,0,0}, 2));
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i = 0; (i < flowerbed.length) && (n > 0); i++) {
+            if(flowerbed[i] == 0) {
+                if((i - 1) >= 0) {
+                    if(flowerbed[i - 1] == 1) {
+                        continue;
+                    }
+                }
+                if((i + 1) < flowerbed.length) {
+                    if(flowerbed[i + 1] == 1) {
+                        continue;
+                    }
+                }
+
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return (n == 0);
     }
 }
