e389c541a5a63b7c4e0c51512f9bed54ccf94a15
Josh <dom.josh2701@gmail.com>
2023-12-22 03:00:33 -0800

    Solve Problem 509 - Fibonacci Number

--- P_0509/p_0509.java
+++ P_0509/p_0509.java
@@ -1,5 +1,22 @@
 public class p_0509 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(fib(2));
+        System.out.println(fib(3));
+        System.out.println(fib(4));
+    }
+
+    public static int fib(int n) {
+        return fib(n, 0, 1);
+    }
+
+    public static int fib(int n, int a, int b) {
+        if(n == 0) {
+            return a;
+        }
+
+        b += a;
+        a = b - a;
+        
+        return fib(--n, a, b);
     }
 }
