6523e3ed19d300dc1a27b9fc863f8a25f0d93d49
Josh <dom.josh2701@gmail.com>
2023-12-21 19:42:50 -0800

    Solve Problem 771 - Jewels and Stones

--- P_0771/p_0771.java
+++ P_0771/p_0771.java
@@ -1,5 +1,17 @@
 public class p_0771 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(numJewelsInStones("aA", "aAAbbbb"));
+        System.out.println(numJewelsInStones("z", "ZZ"));
+        System.out.println(numJewelsInStones("aA", "aAAbbbAbab"));
+    }
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for(char x : stones.toCharArray()) {
+            if(jewels.indexOf(x) != -1) {
+                count++;
+            }
+        }
+        return count;
     }
 }
