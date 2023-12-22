3d54f1b0d830db5480e7fb2823e96f908aaf1ad4
Josh <dom.josh2701@gmail.com>
2023-12-21 20:12:57 -0800

    Solve Problem 746 - Min Cost Climbing Stairs

--- P_0746/p_0746.java
+++ P_0746/p_0746.java
@@ -1,5 +1,28 @@
 public class p_0746 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(minCostClimbingStairs(new int[]{10,15,20}));
+        System.out.println(minCostClimbingStairs(new int[]{1,100,1,1,1,100,1,1,100,1}));
+        System.out.println(minCostClimbingStairs(new int[]{10,15,1}));
+        System.out.println(minCostClimbingStairs(new int[]{10,15}));
+        System.out.println(minCostClimbingStairs(new int[]{0,2,2,1}));
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int[] total_cost = cost.clone();
+        for(int i = 2; i < cost.length; i++) {
+            total_cost[i] = Integer.MAX_VALUE;
+        }
+
+        for(int i = 0; i < cost.length; i++) {
+            if((i + 2) < cost.length) {
+                if((total_cost[i] + cost[i + 1]) < total_cost[i + 1]) {
+                    total_cost[i + 1] = (total_cost[i] + cost[i + 1]);
+                }
+                if((total_cost[i] + cost[i + 2]) < total_cost[i + 2]) {
+                    total_cost[i + 2] = (total_cost[i] + cost[i + 2]);
+                }
+            }
+        }
+        return Integer.min(total_cost[cost.length - 1], total_cost[cost.length - 2]);
     }
 }
