38b56e96a1981ee768ef3da8a97a3fce43ac4313
Josh <dom.josh2701@gmail.com>
2023-12-22 03:29:00 -0800

    Solve Problem 342 - Power of Four

--- P_0342/p_0342.java
+++ P_0342/p_0342.java
@@ -1,5 +1,12 @@
 public class p_0342 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(isPowerOfFour(16));
+        System.out.println(isPowerOfFour(5));
+        System.out.println(isPowerOfFour(1));
+    }
+
+    public static boolean isPowerOfFour(int n) {
+        // Log(x) / Log(4) = Log_4(x)
+        return (((Math.log((double) n) / Math.log(4.0)) % 1) == 0);
     }
 }
