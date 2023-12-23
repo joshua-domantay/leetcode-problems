a26e0f0dd0b297d945161dd55a5d801c9eda7b67
Josh <dom.josh2701@gmail.com>
2023-12-22 17:43:40 -0800

    Solve Problem 441 - Arranging Coins

--- P_0441/p_0441.java
+++ P_0441/p_0441.java
@@ -1,5 +1,16 @@
 public class p_0441 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(arrangeCoins(5));
+        System.out.println(arrangeCoins(8));
+        System.out.println(arrangeCoins(10));
+        System.out.println(arrangeCoins(2147483647)); 
+    }
+
+    public static int arrangeCoins(int n) {
+        for(int i = 1; true; i++) {
+            n -= i;
+            if(n == 0) { return i; }
+            if(n < 0) { return (i - 1); }
+        }
     }
 }
