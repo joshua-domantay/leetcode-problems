f64ce7d47a596b5868af905e3d773dc21c84a7d3
Josh <dom.josh2701@gmail.com>
2023-12-23 04:12:30 -0800

    Solve Problem 977 - Squares of a Sorted Array

--- P_0977/p_0977.java
+++ P_0977/p_0977.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class p_0977 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] result = sortedSquares(new int[]{-4,-1,0,3,10});
+        for(int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " > ");
+        }
+        System.out.println();
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
     }
 }
