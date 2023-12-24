78b65dc15fc40daded2e939315f875ad486ba7f3
Josh <dom.josh2701@gmail.com>
2023-12-24 02:21:14 -0800

    Solve Problem 303 - Range Sum Query - Immutable

--- P_0303/p_0303.java
+++ P_0303/p_0303.java
@@ -1,5 +1,8 @@
 public class p_0303 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        NumArray test = new NumArray(new int[]{-2, 0, 3, -5, 2, -1});
+        System.out.println(test.sumRange(0, 2));
+        System.out.println(test.sumRange(2, 5));
+        System.out.println(test.sumRange(0, 5));
     }
 }
