35128713cb3711d5fbf7b6983e274c34b9bf79af
Josh <dom.josh2701@gmail.com>
2023-12-27 04:37:26 -0800

    Solve Problem 796 - Rotate String

--- P_0796/p_0796.java
+++ P_0796/p_0796.java
@@ -1,5 +1,16 @@
 public class p_0796 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(rotateString("abcde", "cdeab"));
+        System.out.println(rotateString("abcde", "abced"));
+    }
+
+    public static boolean rotateString(String s, String goal) {
+        for(int i = 0; i < goal.length(); i++) {
+            if(s.equals(goal)) {
+                return true;
+            }
+            s = s.substring(1) + s.charAt(0);
+        }
+        return false;
     }
 }
