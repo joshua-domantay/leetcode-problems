7942562952a997df45bb7f6e65b518e3e6bc47ab
Josh <dom.josh2701@gmail.com>
2023-12-28 01:36:35 -0800

    Solve Problem 933 - Number of Recent Calls

--- P_0933/RecentCounter.java
+++ P_0933/RecentCounter.java
@@ -1,9 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RecentCounter {
+    private int[] ranges;
+    private Queue<Integer> calls;
+
     public RecentCounter() {
-        
+        ranges = new int[]{-3000, 0};
+        calls = new LinkedList<Integer>();
     }
     
     public int ping(int t) {
-        return 0;
+        ranges[0] = t - 3000;
+        ranges[1] = t;
+        calls.add(t);
+        while(calls.peek() < ranges[0]) {
+            calls.remove();
+        }
+        return calls.size();
     }
 }
--- P_0933/p_0933.java
+++ P_0933/p_0933.java
@@ -1,5 +1,10 @@
 public class p_0933 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        RecentCounter test = new RecentCounter();
+        System.out.println(test.ping(1));
+        System.out.println(test.ping(100));
+        System.out.println(test.ping(3001));
+        System.out.println(test.ping(3002));
+        System.out.println(test.ping(5000));
     }
 }
