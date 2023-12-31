21e4dff21a54b57b811dcc3de3c6f947d121c7c1
Josh <dom.josh2701@gmail.com>
2023-12-31 04:47:14 -0800

    Solve Problem 817 - Linked List Components

--- P_0817/p_0817.java
+++ P_0817/p_0817.java
@@ -1,5 +1,36 @@
+import java.util.HashSet;
+
 public class p_0817 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ListNode test = new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3))));
+        System.out.println(numComponents(test, new int[]{0,1,3}));
+        test = new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))));
+        System.out.println(numComponents(test, new int[]{0,3,1,4}));
+    }
+
+    public static int numComponents(ListNode head, int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        for(int x : nums) {
+            numSet.add(x);
+        }
+
+        int count = 0;
+        boolean countStart = false;
+        while(head != null) {
+            if(numSet.contains(head.val)) {
+                countStart = true;
+            } else {
+                if(countStart) {
+                    count++;
+                }
+                countStart = false;
+            }
+            head = head.next;
+        }
+        if(countStart) {
+            count++;
+        }
+
+        return count;
     }
 }
