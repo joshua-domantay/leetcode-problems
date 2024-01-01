7b24027183eead429541aeee9cf70d0af93b55bb
Josh <dom.josh2701@gmail.com>
2024-01-01 06:41:31 -0800

    Solve Problem 748 - Shortest Completing Word

--- P_0748/p_0748.java
+++ P_0748/p_0748.java
@@ -1,5 +1,47 @@
+import java.util.HashMap;
+
 public class p_0748 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(shortestCompletingWord("1s3 PSt", new String[]{"step","steps","stripe","stepple"}));
+        System.out.println(shortestCompletingWord("1s3 456", new String[]{"looks","pest","stew","show"}));
+    }
+
+    public static String shortestCompletingWord(String licensePlate, String[] words) {
+        HashMap<Character, Integer> letters = new HashMap<>();
+        for(char i : licensePlate.toLowerCase().toCharArray()) {
+            if(letters.containsKey(i)) {
+                letters.put(i, letters.get(i) + 1);
+            } else {
+                letters.put(i, 1);
+            }
+        }
+
+        HashMap<Character, Integer> lettersCopy = new HashMap<>(letters);   // Copy letters
+        String result = "";
+        int highest = Integer.MIN_VALUE;
+        int count = 0;
+        for(int i = 0; i < words.length; i++) {
+            lettersCopy = new HashMap<>(letters);
+            count = 0;
+            for(char j : words[i].toLowerCase().toCharArray()) {
+                if(lettersCopy.containsKey(j)) {
+                    if(lettersCopy.get(j) > 0) {
+                        lettersCopy.put(j, lettersCopy.get(j) - 1);
+                        count++;
+                    }
+                }
+            }
+            if(count > highest) {
+                result = words[i];
+                highest = count;
+            } else if(count == highest) {
+                if(words[i].length() < result.length()) {
+                    result = words[i];
+                    highest = count;
+                }
+            }
+        }
+
+        return result;
     }
 }
