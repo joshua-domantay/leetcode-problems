d637ad0e31f4482e422849bc675fff7125549925
Josh <dom.josh2701@gmail.com>
2023-12-31 20:33:09 -0800

    Solve Problem 700 - Search in a Binary Search Tree

--- P_0700/p_0700.java
+++ P_0700/p_0700.java
@@ -1,6 +1,17 @@
 public class p_0700 {
     public static void main(String[] args) {
-        TreeNode test = new TreeNode();
-        System.out.println("Hello World!");
+        TreeNode test = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7));
+        TreeNode result = searchBST(test, 2);
+        System.out.println((result == null) ? "_" : result.val);
+        result = searchBST(test, 5);
+        System.out.println((result == null) ? "_" : result.val);
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if((root == null) || (root.val == val)) {
+            return root;
+        }
+
+        return searchBST(((root.val > val) ? root.left : root.right), val);
     }
 }
