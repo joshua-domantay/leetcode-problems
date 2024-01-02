21bfd00d9f22ce936cb3e5daef5745d9dba08bca
Josh <dom.josh2701@gmail.com>
2024-01-02 14:28:23 -0800

    Solve Problem 9 - Palindrome Number

--- P_0009/p_0009.java
+++ P_0009/p_0009.java
@@ -1,5 +1,20 @@
 public class p_0009 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if(x < 0) { return false; }
+
+        String xString = x + "";
+        for(int i = 0; i < xString.length(); i++) {
+            if((xString.charAt(i) - 48) != (x % 10)) {
+                return false;
+            }
+            x /= 10;
+        }
+        return true;
     }
 }
