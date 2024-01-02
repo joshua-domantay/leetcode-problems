fe36ddbb60d814cb10b2e29216a3790e457aaef0
Josh <dom.josh2701@gmail.com>
2024-01-02 06:29:27 -0800

    Solve Problem 1935 - Maximum Number of Words You Can Type

--- P_1935/p_1935.java
+++ P_1935/p_1935.java
@@ -1,5 +1,34 @@
+import java.util.HashSet;
+
 public class p_1935 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(canBeTypedWords("hello world", "ad"));
+        System.out.println(canBeTypedWords("leet code", "lt"));
+        System.out.println(canBeTypedWords("leet code", "e"));
+    }
+
+    public static int canBeTypedWords(String text, String brokenLetters) {
+        int words = 0;
+        HashSet<Character> brokenSet = new HashSet<>();
+        
+        for(char c : brokenLetters.toCharArray()) {
+            brokenSet.add(c);
+        }
+
+        boolean clear;
+        for(String word : text.split(" ")) {
+            clear = true;
+            for(char c : word.toCharArray()) {
+                if(brokenSet.contains(c)) {
+                    clear = false;
+                    break;
+                }
+            }
+            if(clear) {
+                words++;
+            }
+        }
+
+        return words;
     }
 }
