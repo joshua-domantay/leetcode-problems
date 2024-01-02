512be61d5620d8e2b76c779d2331f0c6cef82dc4
Josh <dom.josh2701@gmail.com>
2024-01-02 14:31:28 -0800

    Optimize Problem 9 - Palindrome Number

--- P_0009/p_0009.java
+++ P_0009/p_0009.java
@@ -7,14 +7,13 @@ public class p_0009 {
 
     public static boolean isPalindrome(int x) {
         if(x < 0) { return false; }
+        if((x >= 10) && ((x % 10) == 0)) { return false; }
 
-        String xString = x + "";
-        for(int i = 0; i < xString.length(); i++) {
-            if((xString.charAt(i) - 48) != (x % 10)) {
-                return false;
-            }
-            x /= 10;
+        int xCopy = x, y = 0;
+        while(xCopy > 0) {
+            y = (y * 10) + (xCopy % 10);
+            xCopy /= 10;
         }
-        return true;
+        return x == y;
     }
 }
