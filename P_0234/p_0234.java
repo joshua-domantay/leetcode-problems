988158075eba8c9b054a5695d1baa8ead903a057
Josh <dom.josh2701@gmail.com>
2024-01-02 22:13:29 -0800

    Solve Problem 234 - Palindrome Linked List

--- P_0234/p_0234.java
+++ P_0234/p_0234.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+
 public class p_0234 {
     public static void main(String[] args) {
-        ListNode test = new ListNode();
-        System.out.println("Hello World!");
+        ListNode test = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+        System.out.println(isPalindrome(test));
+        test = new ListNode(1, new ListNode(2));
+        System.out.println(isPalindrome(test));
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        ListNode headCopy = head;
+        while(headCopy != null) {
+            arr.add(headCopy.val);
+            headCopy = headCopy.next;
+        }
+
+        for(int i = (arr.size() - 1); i >= 0; i--) {
+            if(arr.get(i) != head.val) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
     }
 }
