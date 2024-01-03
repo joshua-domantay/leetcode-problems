d5778f5009b13f1b4d2267691a3b87879d84c916
Josh <dom.josh2701@gmail.com>
2024-01-02 23:38:14 -0800

    Attempt to optimize Problem 234 - Palindrome Linked List

--- P_0234/p_0234.java
+++ P_0234/p_0234.java
@@ -2,25 +2,28 @@ import java.util.ArrayList;
 
 public class p_0234 {
     public static void main(String[] args) {
-        ListNode test = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
-        System.out.println(isPalindrome(test));
-        test = new ListNode(1, new ListNode(2));
-        System.out.println(isPalindrome(test));
+        // ListNode test = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+        // System.out.println(isPalindrome(test));
+        // test = new ListNode(1, new ListNode(2));
+        // System.out.println(isPalindrome(test));
+        // test = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(1))));
+        // System.out.println(isPalindrome(test));
+        // test = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1)))));
+        // System.out.println(isPalindrome(test));
     }
 
     public static boolean isPalindrome(ListNode head) {
         ArrayList<Integer> arr = new ArrayList<>();
-        ListNode headCopy = head;
-        while(headCopy != null) {
-            arr.add(headCopy.val);
-            headCopy = headCopy.next;
+        while(head != null) {
+            arr.add(head.val);
+            head = head.next;
         }
 
-        for(int i = (arr.size() - 1); i >= 0; i--) {
-            if(arr.get(i) != head.val) {
+        int arrSize = arr.size();
+        for(int i = 0; i < (arrSize / 2); i++) {
+            if(arr.get(i) != arr.get(arrSize - 1 - i)) {
                 return false;
             }
-            head = head.next;
         }
         return true;
     }
