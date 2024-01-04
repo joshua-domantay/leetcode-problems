f34cee31046be0ffc3fa4828b74681fcab446785
Josh <dom.josh2701@gmail.com>
2024-01-03 22:38:41 -0800

    Solve Problem 2490 - Circular Sentence

--- P_2490/p_2490.java
+++ P_2490/p_2490.java
@@ -1,5 +1,20 @@
 public class p_2490 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(isCircularSentence("leetcode exercises sound delightful"));
+        System.out.println(isCircularSentence("eetcode"));
+        System.out.println(isCircularSentence("Leetcode is cool"));
+        System.out.println(isCircularSentence("Leetcode eisc cool"));
+    }
+
+    public static boolean isCircularSentence(String sentence) {
+        char last = sentence.charAt(sentence.length() - 1);
+        String[] words = sentence.split(" ");
+        for(String x : words) {
+            if(x.charAt(0) != last) {
+                return false;
+            }
+            last = x.charAt(x.length() - 1);
+        }
+        return true;
     }
 }
