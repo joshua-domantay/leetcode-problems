d23661cf1754f9ec628f873033f9411a7887bb69
Josh <dom.josh2701@gmail.com>
2024-01-04 04:25:28 -0800

    Solve Problem 2784 - Check if Array is Good

--- P_2784/p_2784.java
+++ P_2784/p_2784.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class p_2784 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(isGood(new int[]{2, 1, 4, 4, 3}));
+    }
+
+    public static boolean isGood(int[] nums) {
+        ArrayList<Integer> base = new ArrayList<>();
+        for(int i = 0; i < (nums.length - 1); i++) {
+            base.add(i + 1);
+        }
+        base.add(nums.length - 1);
+
+        for(int x : nums) {
+            if(!base.contains(x)) {
+                return false;
+            }
+            base.remove(Integer.valueOf(x));
+        }
+
+        return true;
     }
 }
