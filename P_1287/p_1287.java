da57757d79423fd524d4c33b3bca68e5cdf47676
Josh <dom.josh2701@gmail.com>
2024-01-05 04:19:09 -0800

    Solve Problem 1287 - Element Appearing More Than 25% In Sorted Array

--- P_1287/p_1287.java
+++ P_1287/p_1287.java
@@ -1,5 +1,24 @@
 public class p_1287 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(findSpecialInteger(new int[]{1,2,2,6,6,6,6,7,10}));
+        System.out.println(findSpecialInteger(new int[]{1,1}));
+        System.out.println(findSpecialInteger(new int[]{1,1}));
+        System.out.println(findSpecialInteger(new int[]{1,1,1,5}));
+        System.out.println(findSpecialInteger(new int[]{1,5,5,7}));
+    }
+
+    public static int findSpecialInteger(int[] arr) {
+        int last = arr[0], count = 0;
+        for(int num : arr) {
+            if(num != last) {
+                last = num;
+                count = 0;
+            }
+            count++;
+            if(count > (arr.length / 4)) {
+                return num;
+            }
+        }
+        return last;
     }
 }
