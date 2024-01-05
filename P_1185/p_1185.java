9a064da72c87c49ee7e74a11db373da4bbcdae0d
Josh <dom.josh2701@gmail.com>
2024-01-04 19:59:11 -0800

    Solve Problem 1185 - Day of the Week

--- P_1185/p_1185.java
+++ P_1185/p_1185.java
@@ -1,5 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class p_1185 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(dayOfTheWeek(31, 8, 2019));
+        System.out.println(dayOfTheWeek(18, 7, 1999));
+        System.out.println(dayOfTheWeek(15, 8, 1993));
+        System.out.println(dayOfTheWeek(16, 8, 1993));
+        System.out.println(dayOfTheWeek(17, 8, 1993));
+        System.out.println(dayOfTheWeek(4, 1, 2024));
+    }
+
+    public static String dayOfTheWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("E");
+        switch(date.format(format)) {
+            case "Sun":
+                return "Sunday";
+            case "Mon":
+                return "Monday";
+            case "Tue":
+                return "Tuesday";
+            case "Wed":
+                return "Wednesday";
+            case "Thu":
+                return "Thursday";
+            case "Fri":
+                return "Friday";
+            default:    // Sat
+                return "Saturday";
+        }
     }
 }
