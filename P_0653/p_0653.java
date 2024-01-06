45fcc3b1f3a520b4472c9e50b5a227ca9eba64ee
Josh <dom.josh2701@gmail.com>
2024-01-06 00:49:21 -0800

    Optimize 20ms to 2ms Problem 653 - Two Sum IV - Input is a BST

--- P_0653/p_0653.java
+++ P_0653/p_0653.java
@@ -7,22 +7,45 @@ public class p_0653 {
         System.out.println(findTarget(test, 28));
     }
 
+    public static void inorder(TreeNode curr, ArrayList<Integer> values) {
+        if(curr == null) { return; }
+        inorder(curr.left, values);
+        values.add(curr.val);
+        inorder(curr.right,values);
+    }
+
     public static boolean findTarget(TreeNode root, int k) {
         ArrayList<Integer> values = new ArrayList<>();
-        ArrayList<TreeNode> breadth = new ArrayList<>();
-        breadth.add(root);
-        TreeNode curr;
-        do {
-            curr = breadth.remove(0);   // pop
-            if(curr != null) {
-                if(values.contains(k - curr.val)) {
-                    return true;
-                }
-                values.add(curr.val);
-                breadth.add(curr.left);
-                breadth.add(curr.right);
+        inorder(root, values);
+        int start = 0, end = (values.size() - 1), sum;
+        while(start < end) {
+            sum = (values.get(start) + values.get(end));
+            if(sum == k) {
+                return true;
             }
-        } while(breadth.size() > 0);
+            if(sum < k) {
+                start++;
+            } else {
+                end--;
+            }
+        }
         return false;
+
+        // ArrayList<Integer> values = new ArrayList<>();
+        // ArrayList<TreeNode> breadth = new ArrayList<>();
+        // breadth.add(root);
+        // TreeNode curr;
+        // do {
+        //     curr = breadth.remove(0);   // pop
+        //     if(curr != null) {
+        //         if(values.contains(k - curr.val)) {
+        //             return true;
+        //         }
+        //         values.add(curr.val);
+        //         breadth.add(curr.left);
+        //         breadth.add(curr.right);
+        //     }
+        // } while(breadth.size() > 0);
+        // return false;
     }
 }
