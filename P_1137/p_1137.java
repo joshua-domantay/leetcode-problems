cf3655917e29f43cdd30b032cd16028eb4d3ff72
Josh <dom.josh2701@gmail.com>
2024-01-07 00:45:58 -0800

    Solve Problem 1137 - N-th Tribonacci Number

--- P_1137/p_1137.java
+++ P_1137/p_1137.java
@@ -1,5 +1,21 @@
 public class p_1137 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(tribonacci(4));
+        System.out.println(tribonacci(5));
+        System.out.println(tribonacci(25));
+    }
+
+    public static int tribonacci(int n) {
+        if(n < 2) { return n; }
+
+        int a = 0, b = 1, c = 1, temp;
+        while(n > 2) {
+            temp = c;
+            c += a + b;
+            a = b;
+            b = temp;
+            n--;
+        }
+        return c;
     }
 }
