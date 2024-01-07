c733cd744067a27f6afe0b85e29ef37d40364faa
Josh <dom.josh2701@gmail.com>
2024-01-06 23:31:48 -0800

    Solve Problem 704 - Binary Search

--- P_0704/p_0704.java
+++ P_0704/p_0704.java
@@ -1,5 +1,22 @@
 public class p_0704 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(search(new int[]{-1,0,3,5,9,12}, 9));
+        System.out.println(search(new int[]{-1,0,3,5,9,12}, 2));
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0, mid, end = nums.length;
+        while((start + 1) < end) {
+            mid = (end + start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(target < nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        return (nums[start] == target) ? start : -1;
     }
 }
