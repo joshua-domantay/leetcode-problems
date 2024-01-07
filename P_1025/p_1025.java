324ce7308bbb138f4c13738b53853740ff97fbac
Josh <dom.josh2701@gmail.com>
2024-01-06 23:46:48 -0800

    Solve Problem 1025 - Divisor Game

--- P_1025/p_1025.java
+++ P_1025/p_1025.java
@@ -1,5 +1,21 @@
 public class p_1025 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(divisorGame(2));
+        System.out.println(divisorGame(3));
+        System.out.println(divisorGame(657));
+        System.out.println(divisorGame(658));
+    }
+
+    public static boolean divisorGame(int n) {
+        return (n % 2) == 0;
+        // int x, turn = 0;
+        // while(n > 1) {
+        //     if(n % 2 == 0) {    // even
+                
+        //     } else {    // odd
+        //         n -= 1;
+        //     }
+        //     turn++;
+        // }
     }
 }
