9a53703c223e36dcc47b8134e155b16ac8ce6865
Josh <dom.josh2701@gmail.com>
2024-01-06 22:48:06 -0800

    Solve Problem 897 - Increasing Order Search Tree

--- P_0897/p_0897.java
+++ P_0897/p_0897.java
@@ -1,5 +1,44 @@
+import java.util.ArrayList;
+
 public class p_0897 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        TreeNode test = new TreeNode(5, new TreeNode(3, new TreeNode(2, new TreeNode(1), null), new TreeNode(4)), new TreeNode(6, null, new TreeNode(8, new TreeNode(7), new TreeNode(9))));
+        printResult(increasingBST(test));
+        test = new TreeNode(5, new TreeNode(1), new TreeNode(7));
+        printResult(increasingBST(test));
+    }
+
+    public static void printResult(TreeNode n) {
+        while(n != null) {
+            System.out.print(n.val);
+            System.out.print((n.right == null) ? "" : ", ");
+            n = n.right;
+        }
+        System.out.println();
+    }
+
+    public static void inorder(TreeNode n, ArrayList<TreeNode> values) {
+        if(n == null) { return; }
+
+        inorder(n.left, values);
+        values.add(n);
+        inorder(n.right, values);
+
+        n.left = null;
+        n.right = null;
+    }
+
+    public static TreeNode increasingBST(TreeNode root) {
+        ArrayList<TreeNode> values = new ArrayList<>();
+        inorder(root, values);
+
+        TreeNode result = values.remove(0);
+        TreeNode temp = result;
+        while(values.size() > 0) {
+            temp.right = values.remove(0);
+            temp = temp.right;
+        }
+
+        return result;
     }
 }
