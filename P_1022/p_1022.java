d6a363453b1650060ad5ab9b303461b986dc8507
Josh <dom.josh2701@gmail.com>
2024-01-07 07:08:56 -0800

    Solve Problem 1022 - Sum of Root To Leaf Binary Numbers

--- P_1022/p_1022.java
+++ P_1022/p_1022.java
@@ -1,6 +1,28 @@
 public class p_1022 {
     public static void main(String[] args) {
-        TreeNode test;
-        System.out.println("Hello World!");
+        TreeNode test = new TreeNode(1, new TreeNode(0, new TreeNode(0), new TreeNode(1)), new TreeNode(1, new TreeNode(0), new TreeNode(1)));
+        System.out.println(sumRootToLeaf(test));
+        test = new TreeNode(0);
+        System.out.println(sumRootToLeaf(test));
+        test = new TreeNode(1, new TreeNode(0, null, new TreeNode(1)), new TreeNode(1, new TreeNode(0), new TreeNode(1)));
+        System.out.println(sumRootToLeaf(test));
+    }
+
+    public static int sumRootToLeaf(TreeNode root) {
+        if((root.left == null) && (root.right == null)) {
+            return root.val;
+        }
+
+        int sum = 0;
+        root.val = root.val << 1;
+        if(root.left != null) {
+            root.left.val = (root.val | root.left.val);
+            sum += sumRootToLeaf(root.left);
+        }
+        if(root.right != null) {
+            root.right.val = (root.val | root.right.val);
+            sum += sumRootToLeaf(root.right);
+        }
+        return sum;
     }
 }
