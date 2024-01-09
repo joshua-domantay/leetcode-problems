073dd5807d5ddf5fcf02518d117333da42051207
Josh <dom.josh2701@gmail.com>
2024-01-09 02:35:39 -0800

    Solve Problem 2980 - Check if Bitwise OR Has Trailing Zeros

--- P_2980/p_2980.java
+++ P_2980/p_2980.java
@@ -1,5 +1,20 @@
 public class p_2980 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(hasTrailingZeros(new int[]{1,2,3,4,5}));
+        System.out.println(hasTrailingZeros(new int[]{2,4,8,16}));
+        System.out.println(hasTrailingZeros(new int[]{1,3,5,7,9}));
+    }
+
+    public static boolean hasTrailingZeros(int[] nums) {
+        int zeros = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if((nums[i] & 1) == 0) {
+                zeros++;
+                if(zeros >= 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
