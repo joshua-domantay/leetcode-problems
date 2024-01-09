2c9ee945f6c1d8948f1f63deb737eb3e2b30374f
Josh <dom.josh2701@gmail.com>
2024-01-08 19:27:11 -0800

    Solve Problem 1374 - Generate a String With Characters That Have Odd Counts

--- P_1374/p_1374.java
+++ P_1374/p_1374.java
@@ -1,5 +1,31 @@
 public class p_1374 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(generateTheString(3));
+        System.out.println(generateTheString(6));
+    }
+
+    public static String generateTheString(int n) {
+        char[] result = new char[n];
+        int index = 0;
+        if(n % 2 == 0) {
+            result[index] = 'b';
+            index++;
+        }
+        while(index < n) {
+            result[index] = 'a';
+            index++;
+        }
+        return new String(result);
+
+        // String result = "";
+        // int limit = n;
+        // if(n % 2 == 0) {
+        //     limit--;
+        //     result += "b";
+        // }
+        // for(int i = 0; i < limit; i++) {
+        //     result += "a";
+        // }
+        // return result;
     }
 }
