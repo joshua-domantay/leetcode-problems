e4a66ca981e723ef93ab65fa6a0964d80eb20b88
Josh <dom.josh2701@gmail.com>
2024-01-14 18:19:57 -0800

    Solve Problem 1603 - Design Parking System

--- P_1603/p_1603.java
+++ P_1603/p_1603.java
@@ -1,5 +1,9 @@
 public class p_1603 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ParkingSystem ps = new ParkingSystem(1, 1, 0);
+        System.out.println(ps.addCar(1));
+        System.out.println(ps.addCar(2));
+        System.out.println(ps.addCar(3));
+        System.out.println(ps.addCar(1));
     }
 }
