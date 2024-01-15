bf49536d8783e36a66330a3e57a42ac19e7e7b81
Josh <dom.josh2701@gmail.com>
2024-01-14 18:51:36 -0800

    Solve Problem 1876 - Substrings of Size Three with Distinct Characters

--- P_1876/p_1876.java
+++ P_1876/p_1876.java
@@ -1,5 +1,25 @@
+import java.util.HashSet;
+
 public class p_1876 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(countGoodSubstrings("xyzzaz"));
+        System.out.println(countGoodSubstrings("aababcabc"));
+    }
+
+    public static int countGoodSubstrings(String s) {
+        int result = 0;
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0; i < (s.length() - 2); i++) {
+            for(int j = 0; j < 3; j++) {
+                char x = s.charAt(i + j);
+                if(set.contains(x)) {
+                    break;
+                }
+                set.add(x);
+            }
+            if(set.size() == 3) { result++; }
+            set.clear();
+        }
+        return result;
     }
 }
