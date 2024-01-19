c400e52ebb3ec5b04684358add2539fa730669d1
Josh <dom.josh2701@gmail.com>
2024-01-18 19:32:05 -0800

    Optimize Problem 865 - Smallest Subtree with all the Deepest Nodes

--- P_0865/p_0865.java
+++ P_0865/p_0865.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class p_0865 {
+    private static TreeNode subtree;
+    private static int deepest = -1;
+
     public static void main(String[] args) {
         TreeNode test = new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))), new TreeNode(1, new TreeNode(0), new TreeNode(8)));
         printTreeNode(subtreeWithAllDeepest(test));
@@ -28,56 +26,32 @@ public class p_0865 {
     }
 
     public static TreeNode subtreeWithAllDeepest(TreeNode root) {
-        HashMap<Integer, ArrayList<TreeNode>> depthToValue = new HashMap<>();
-        HashMap<Integer, TreeNode> valueParent = new HashMap<>();
-        depthToValue.put(0, new ArrayList<>());
-        Queue<TreeNode> stack = new LinkedList<>();
-        stack.add(root);
-        stack.add(null);
+        // Comment out next 2 lines for leetcode
+        subtree = null;
+        deepest = -1;
 
-        TreeNode curr;
-        int depth = 0;
-        do {
-            curr = stack.poll();
+        getSubtree(root, 0);
+        return subtree;
+    }
 
-            if(curr == null) {
-                if(stack.size() != 0) {
-                    stack.add(null);
-                    depth++;
-
-                    depthToValue.put(depth, new ArrayList<>());
-                }
-
-                continue;
+    public static int getSubtree(TreeNode root, int depth) {
+        if(root == null) { return (depth - 1); }
+        if((root.left == null) && (root.right == null)) {
+            if(depth > deepest) {
+                subtree = root;
+                deepest = depth;
             }
+            return depth;
+        }
 
-            if(curr.left != null) {
-                valueParent.put(curr.left.val, curr);
-                stack.add(curr.left);
-            }
-            if(curr.right != null) {
-                valueParent.put(curr.right.val, curr);
-                stack.add(curr.right);
-            }
+        depth++;
+        int left = getSubtree(root.left, depth);
+        int right = getSubtree(root.right, depth);
 
-            depthToValue.get(depth).add(curr);
-        } while(stack.size() > 0);
+        if((left == right) && (left == deepest)) {
+            subtree = root;
+        }
 
-        if(depthToValue.get(depth).size() > 1) {
-            for(TreeNode i : depthToValue.get(depth)) {
-                stack.add(i);
-            }
-
-            while(stack.size() > 1) {
-                curr = stack.poll();
-                if(!stack.contains(valueParent.get(curr.val))) {
-                    stack.add(valueParent.get(curr.val));
-                }
-            }
-
-            return stack.poll();
-        }    
-
-        return depthToValue.get(depth).get(0);
+        return Math.max(left, right);
     }
 }
