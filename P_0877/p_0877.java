70a0f831e1c5be56bdbe76cf401fb563dd7f6c7a
Josh <dom.josh2701@gmail.com>
2024-01-20 02:55:27 -0800

    Solve Problem - 877 Stone Game

--- P_0877/p_0877.java
+++ P_0877/p_0877.java
@@ -1,5 +1,9 @@
 public class p_0877 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(stoneGame(new int[]{1,2}));
+    }
+
+    public static boolean stoneGame(int[] piles) {
+        return true;
     }
 }
