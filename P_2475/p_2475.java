9f064842fa5fab5d888161af212651860329170e
Josh <dom.josh2701@gmail.com>
2024-01-20 23:14:07 -0800

    Solve Problem 2475 - Number of Unequal Triples in Array

--- P_2475/p_2475.java
+++ P_2475/p_2475.java
@@ -1,5 +1,22 @@
 public class p_2475 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(unequalTriplets(new int[]{4,4,2,4,3}));
+        System.out.println(unequalTriplets(new int[]{1,1,1,1,1}));
+    }
+
+    public static int unequalTriplets(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] != nums[j]) {
+                    for(int k = j + 1; k < nums.length; k++) {
+                        if((nums[i] != nums[k]) && (nums[j] != nums[k])) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
