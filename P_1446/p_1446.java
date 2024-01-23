42f04c6154b45137f3f764448b3b5dfca3f56c8c
Josh <dom.josh2701@gmail.com>
2024-01-23 02:52:13 -0800

    Solve Problem 1446 - Consecutive Characters

--- P_1446/p_1446.java
+++ P_1446/p_1446.java
@@ -1,5 +1,24 @@
 public class p_1446 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(maxPower("leetcode"));
+        System.out.println(maxPower("abbcccddddeeeeedcba"));
+    }
+
+    public static int maxPower(String s) {
+        char curr = '-';
+        int count = 0, max = -1;
+        for(char c : s.toCharArray()) {
+            if(c != curr) {
+                if(count > max) { max = count; }
+                curr = c;
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+
+        if(count > max) { max = count; }
+
+        return max;
     }
 }
