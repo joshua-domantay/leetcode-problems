c6f26e43806a87847d759a0be8ff8c804bf1bbd6
Josh <dom.josh2701@gmail.com>
2024-01-23 21:10:30 -0800

    Solve Problem 1413 - Minimum Value to Get Positive Step by Step Sum

--- P_1413/p_1413.java
+++ P_1413/p_1413.java
@@ -1,5 +1,39 @@
 public class p_1413 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(minStartValue(new int[]{-3,2,-3,4,2}));
+        System.out.println(minStartValue(new int[]{1,2}));
+        System.out.println(minStartValue(new int[]{1,-2,-3}));
+    }
+
+    public static int minStartValue(int[] nums) {
+        /* 0ms */
+        int val = 1, startVal;
+        if(nums[0] < 0) {
+            val = (nums[0] * -1) + 1;
+        }
+        startVal = val;
+
+        for(int num : nums) {
+            startVal = startVal + num;
+            if(startVal < 1) {
+                val += (startVal * -1) + 1;
+                startVal = 1;
+            }
+        }
+
+        return val;
+
+        /* 2ms */
+        // int val = 1, startVal;
+        // while(true) {
+        //     startVal = val;
+        //     for(int num : nums) {
+        //         startVal = startVal + num;
+        //         if(startVal < 1) { break; }
+        //     }
+        //     if(startVal >= 1) { break; }
+        //     startVal = ++val;
+        // }
+        // return val;
     }
 }
