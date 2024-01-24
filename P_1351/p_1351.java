9b47aa5f5c7104545280e227c2081fd55d711fd9
Josh <dom.josh2701@gmail.com>
2024-01-24 03:51:25 -0800

    Solve Problem 1351 - Count Negative Numbers in a Sorted Matrix

--- P_1351/p_1351.java
+++ P_1351/p_1351.java
@@ -1,5 +1,37 @@
 public class p_1351 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(countNegatives(new int[][]{{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}}));
+        System.out.println(countNegatives(new int[][]{{3,2},{1,0}}));
+    }
+
+    public static int countNegatives(int[][] grid) {
+        int count = 0;
+        boolean hasNegative;
+        for(int i = grid.length - 1; i >= 0; i--) {
+            hasNegative = false;
+            for(int j = grid[i].length - 1; j >= 0; j--) {
+                if(grid[i][j] < 0) {
+                    count++;
+                    hasNegative = true;
+                } else {
+                    break;
+                }
+            }
+            if(!hasNegative) { break; }
+        }
+        return count;
+
+
+
+        /* 1ms for row wise */
+        // int count = 0, j;
+        // for(int i = 0; i < grid.length; i++) {
+        //     j = grid[i].length - 1;
+        //     while((j >= 0) && (grid[i][j] < 0)) {
+        //         count++;
+        //         j--;
+        //     }
+        // }
+        // return count;
     }
 }
