27aaa7e813403e8be195f125bdb10d71959f8f69
Josh <dom.josh2701@gmail.com>
2024-01-27 06:12:47 -0800

    Start Problem 1789 - Primary Department for Each Employee

---
+++ P_1789/p_1789.sql
@@ -0,0 +1,22 @@
+-- Problem 1789 - Primary Department for Each Employee
+-- MySQL
+
+
+-- Table Schema
+USE leetcode_problems   -- Use leetcode_problems database
+Create table If Not Exists Employee (employee_id int, department_id int, primary_flag ENUM('Y','N'));
+Truncate table Employee;
+insert into Employee (employee_id, department_id, primary_flag) values ('1', '1', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('2', '1', 'Y');
+insert into Employee (employee_id, department_id, primary_flag) values ('2', '2', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('3', '3', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '2', 'N');
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '3', 'Y');
+insert into Employee (employee_id, department_id, primary_flag) values ('4', '4', 'N');
+
+-- Code
+SELECT * FROM Employee;
+
+
+-- Drop after run
+DROP TABLE IF EXISTS Employee;
