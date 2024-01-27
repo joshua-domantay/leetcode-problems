b33b20c35341192364282e5e77384ad46c88cd93
Josh <dom.josh2701@gmail.com>
2024-01-27 01:03:52 -0800

    Convert .txt to .sql for Problem 1757 - Recyclable and Low Fat Products

---
+++ P_1757/p_1757.sql
@@ -0,0 +1,22 @@
+-- Problem 1757 - Recyclable and Low Fat Products
+-- MySQL
+
+
+-- Table Schema
+USE leetcode_problems   -- Use leetcode_problems database
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'));
+Truncate table Products;
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N');
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N');
+
+
+-- Code
+SELECT product_id FROM products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+
+-- Drop after run
+DROP TABLE IF EXISTS Products;
