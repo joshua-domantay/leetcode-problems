8f0813b8207d430c31885d26dd611841478da210
Josh <dom.josh2701@gmail.com>
2024-01-27 06:44:08 -0800

    Solve Problem 1789 - Primary Department for Each Employee

--- P_1789/p_1789.sql
+++ P_1789/p_1789.sql
@@ -15,7 +15,16 @@ insert into Employee (employee_id, department_id, primary_flag) values ('4', '3'
 insert into Employee (employee_id, department_id, primary_flag) values ('4', '4', 'N');
 
 -- Code
-SELECT * FROM Employee;
+SELECT employee_id, department_id FROM Employee
+WHERE primary_flag = 'Y'
+UNION
+SELECT employee_id, department_id FROM Employee
+WHERE employee_id IN (
+    SELECT employee_id FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(employee_id) = 1
+)
+ORDER BY employee_id;
 
 
 -- Drop after run
