fef8402c0218d68d369af66e7204f882bb64c3b0
Josh <dom.josh2701@gmail.com>
2024-01-27 20:58:24 -0800

    Start Problem 1581 - Customer Who Visited but Did Not Make Any Transactions

---
+++ P_1581/p_1581.sql
@@ -0,0 +1,31 @@
+-- Problem 1581 - Customer Who Visited but Did Not Make Any Transactions
+-- MySQL
+
+
+-- Table Schema
+USE leetcode_problems   -- Use leetcode_problems database
+Create table If Not Exists Visits(visit_id int, customer_id int);
+Create table If Not Exists Transactions(transaction_id int, visit_id int, amount int);
+Truncate table Visits;
+insert into Visits (visit_id, customer_id) values ('1', '23');
+insert into Visits (visit_id, customer_id) values ('2', '9');
+insert into Visits (visit_id, customer_id) values ('4', '30');
+insert into Visits (visit_id, customer_id) values ('5', '54');
+insert into Visits (visit_id, customer_id) values ('6', '96');
+insert into Visits (visit_id, customer_id) values ('7', '54');
+insert into Visits (visit_id, customer_id) values ('8', '54');
+Truncate table Transactions;
+insert into Transactions (transaction_id, visit_id, amount) values ('2', '5', '310');
+insert into Transactions (transaction_id, visit_id, amount) values ('3', '5', '300');
+insert into Transactions (transaction_id, visit_id, amount) values ('9', '5', '200');
+insert into Transactions (transaction_id, visit_id, amount) values ('12', '1', '910');
+insert into Transactions (transaction_id, visit_id, amount) values ('13', '2', '970');
+
+-- Code
+SELECT * FROM Visits;
+SELECT * FROM Transactions;
+
+
+-- Drop after run
+DROP TABLE IF EXISTS Visits;
+DROP TABLE IF EXISTS Transactions;
