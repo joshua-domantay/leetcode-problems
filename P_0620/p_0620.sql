a3495b199064cd852e4d1aa5a8b32cdcc457adb2
Josh <dom.josh2701@gmail.com>
2024-01-27 20:51:16 -0800

    Start Problem 620 - Not Boring Movies

---
+++ P_0620/p_0620.sql
@@ -0,0 +1,20 @@
+-- Problem 620 - Not Boring Movies
+-- MySQL
+
+
+-- Table Schema
+USE leetcode_problems   -- Use leetcode_problems database
+Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1));
+Truncate table cinema;
+insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9');
+insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5');
+insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2');
+insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6');
+insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1');
+
+-- Code
+SELECT * FROM cinema;
+
+
+-- Drop after run
+DROP TABLE IF EXISTS Cinema;
