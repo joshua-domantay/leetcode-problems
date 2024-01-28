c34bf285d3520ee6df23f440d16a1ff147ecd26f
Josh <dom.josh2701@gmail.com>
2024-01-27 20:43:00 -0800

    Solve Problem 181 - Employees Earning More Than Their Managers

--- P_0181/p_0181.sql
+++ P_0181/p_0181.sql
@@ -12,7 +12,16 @@ insert into Employee (id, name, salary, managerId) values (3, 'Sam', '60000', Nu
 insert into Employee (id, name, salary, managerId) values (4, 'Max', '90000', Null);
 
 -- Code
-SELECT * FROM Employee;
+SELECT t1.name FROM Employee AS t1
+JOIN (
+    SELECT id, name, salary FROM Employee
+    WHERE id IN (
+        SELECT managerId FROM Employee
+        WHERE managerId % 1 = 0
+    )
+) AS t2
+ON t1.managerId = t2.id
+AND t1.salary > t2.salary;
 
 
 -- Drop after run
