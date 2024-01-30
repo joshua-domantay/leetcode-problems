99b1b11cd1515f69063e78b60600107317151857
Josh <dom.josh2701@gmail.com>
2024-01-30 03:02:16 -0800

    Solve Problem 1869 - Longer Contiguous Segments of Ones than Zeros

--- P_1869/p_1869.java
+++ P_1869/p_1869.java
@@ -1,5 +1,26 @@
 public class p_1869 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(checkZeroOnes("1101"));
+        System.out.println(checkZeroOnes("111000"));
+        System.out.println(checkZeroOnes("110100010"));
+    }
+
+    public static boolean checkZeroOnes(String s) {
+        char last = s.charAt(0);
+        int count = 0;
+        int[] segments = new int[2];
+        for(char c : s.toCharArray()) {
+            if(c == last) {
+                count++;
+            } else {
+                segments[last - '0'] = Math.max(segments[last - '0'], count);
+
+                last = c;
+                count = 1;
+            }
+        }
+        segments[last - '0'] = Math.max(segments[last - '0'], count);
+
+        return segments[1] > segments[0];
     }
 }
