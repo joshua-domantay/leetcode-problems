d4636aa781d5caff5f4634847bbdaec1669db7a5
Josh <dom.josh2701@gmail.com>
2024-02-04 06:03:12 -0800

    Solve Problem 2119 - A Number After a Double Reversal

--- P_2119/p_2119.java
+++ P_2119/p_2119.java
@@ -1,5 +1,12 @@
 public class p_2119 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(isSameAfterReversals(526));
+        System.out.println(isSameAfterReversals(1800));
+        System.out.println(isSameAfterReversals(0));
+    }
+
+    public static boolean isSameAfterReversals(int num) {
+        if((num > 0) && ((num % 10) == 0)) { return false; }
+        return true;
     }
 }
