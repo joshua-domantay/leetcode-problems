a5818b13c02bbbe16a5320807ae8ea1a5f9b2640
Josh <dom.josh2701@gmail.com>
2024-02-04 19:56:59 -0800

    Solve Problem 1464 - Maximum Product of Two Elements in an Array

--- P_1464/p_1464.java
+++ P_1464/p_1464.java
@@ -1,5 +1,18 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class p_1464 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(maxProduct(new int[]{3,4,5,2}));
+        System.out.println(maxProduct(new int[]{1,5,4,5}));
+        System.out.println(maxProduct(new int[]{3,7}));
+    }
+
+    public static int maxProduct(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int num : nums) {
+            pq.add(num);
+        }
+        return ((pq.poll() - 1) * (pq.poll() - 1));
     }
 }
