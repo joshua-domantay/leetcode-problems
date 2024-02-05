d9ef93464f4244571047b819b5e13af2998af582
Josh <dom.josh2701@gmail.com>
2024-02-04 23:05:33 -0800

    Start Problem 1179 - Reformat Department Table

---
+++ P_1179/p_1179.sql
@@ -0,0 +1,20 @@
+-- Problem 1179 - Reformat Department Table
+-- MySQL
+
+
+-- Table Schema
+USE leetcode_problems   -- Use leetcode_problems database
+Create table If Not Exists Department (id int, revenue int, month varchar(5));
+Truncate table Department;
+insert into Department (id, revenue, month) values ('1', '8000', 'Jan');
+insert into Department (id, revenue, month) values ('2', '9000', 'Jan');
+insert into Department (id, revenue, month) values ('3', '10000', 'Feb');
+insert into Department (id, revenue, month) values ('1', '7000', 'Feb');
+insert into Department (id, revenue, month) values ('1', '6000', 'Mar');
+
+-- Code
+SELECT * FROM Department;
+
+
+-- Drop after run
+DROP TABLE IF EXISTS Department;
