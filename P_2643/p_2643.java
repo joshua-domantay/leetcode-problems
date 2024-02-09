c63d3fbd4c922c8505e1f838aeb00fb5c6c4b8d7
Josh <dom.josh2701@gmail.com>
2024-02-09 03:44:28 -0800

    Solve Problem 2643 - Row With Maximum Ones

--- P_2643/p_2643.java
+++ P_2643/p_2643.java
@@ -1,5 +1,28 @@
 public class p_2643 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        printArray(rowAndMaximumOnes(new int[][]{{0,1},{1,0}}));
+        printArray(rowAndMaximumOnes(new int[][]{{0,0,0},{0,1,1}}));
+        printArray(rowAndMaximumOnes(new int[][]{{0,0},{1,1},{0,0}}));
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.println("[" + arr[0] + ", " + arr[1] + "]");
+    }
+
+    public static int[] rowAndMaximumOnes(int[][] mat) {
+        int[] result = new int[2];
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 1; j < mat[i].length; j++) {
+                mat[i][0] += mat[i][j];
+            }
+
+            if(mat[i][0] > result[1]) {
+                result[0] = i;
+                result[1] = mat[i][0];
+            }
+
+            if(mat[i][0] == mat[i].length) { return result; }
+        }
+        return result;
     }
 }
