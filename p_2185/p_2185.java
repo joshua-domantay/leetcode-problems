f5d41cec8af5d639e72e4c0197cec16f06796a20
Josh <dom.josh2701@gmail.com>
2024-02-25 08:18:27 -0800

    Solve Problem 2185 - Counting Words With a Given Prefix

--- p_2185/p_2185.java
+++ p_2185/p_2185.java
@@ -1,5 +1,16 @@
 public class p_2185 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(prefixCount(new String[]{"pay","attention","practice","attend"}, "at"));
+        System.out.println(prefixCount(new String[]{"leetcode","win","loops","success"}, "code"));
+    }
+
+    public static int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for(String word : words) {
+            if(word.length() >= pref.length()) {
+                if(word.substring(0, pref.length()).equals(pref)) { count++; }
+            }
+        }
+        return count;
     }
 }
