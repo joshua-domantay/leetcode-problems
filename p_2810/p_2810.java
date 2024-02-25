c50a3aa5020d201d98e0c729750b992a4cf1bc41
Josh <dom.josh2701@gmail.com>
2024-02-25 08:12:08 -0800

    Solve Problem 2810 - Faulty Keyboard

--- p_2810/p_2810.java
+++ p_2810/p_2810.java
@@ -1,5 +1,18 @@
 public class p_2810 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(finalString("string"));
+        System.out.println(finalString("poiinter"));
+    }
+
+    public static String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(c == 'i') {
+                sb.reverse();
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
